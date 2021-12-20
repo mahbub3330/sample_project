87d2d0e83ab9cd1707c1c818fabded9779b124bc
mahbub3330 <m.alam3330@gmail.com>
2021-12-20 22:00:15 +0600

    task 2 for code moved from controller to formatoutputservice

--- app/Http/Controllers/FetchOutputController.php
+++ app/Http/Controllers/FetchOutputController.php
@@ -2,25 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\FormatOutputService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
 class FetchOutputController extends Controller
 {
-    const API_NAME_PREFIX = [
-        'property_name' => 'properties',
-        'property_address' => 'properties',
-        'tenure_id' => 't',
-        'property_category_id' => 'pc',
-        'building_class_id' => 'bc'
-    ];
-
-    const API_NAME = [
-        'tenure_id' => 'tenure_name',
-        'property_category_id' => 'property_category_name',
-        'building_class_id' => 'building_class_name',
-    ];
-
 
     public function fetchDataForInput(): string
     {
@@ -34,51 +21,42 @@ class FetchOutputController extends Controller
 
         $data = collect($data)->collapse();
 
-        $output = '(';
 
-        foreach ($data as $index => $item) {
-            if (is_array($item)) {
-                $this->formatArrayValue($item, $output);
-            }
-            $output = $this->getString($item, $output);
-        }
 
-        $output .= ')';
-
-        return $output;
+        return FormatOutputService::formatData($data);
 
     }
 
-    public function formatArrayValue($arrayItems, &$output): string
-    {
-        $output .= '(';
-        foreach ($arrayItems as $arrayIndex => $item) {
-            $output = $this->getString($item, $output);
-            if (is_array($item)) {
-                $this->formatArrayValue($item, $output);
-            }
-
-        }
-        $output .= ')';
-
-        return $output;
-    }
-
-
-    public function getString($item, string $output): string
-    {
-        if (is_string($item)) {
-            $output .= ' ' . $item . ' ';
-        }
-        if (is_object($item)) {
-            $api_name_prefix = self::API_NAME_PREFIX[$item->api_name] ?? $item->api_name;
-            $api_name = self::API_NAME[$item->api_name] ?? $item->api_name;
-            $value = $item->comparator == 'contains' ? ' ' . '\'%' . $item->value . '%\'' : '';
-
-            $output .= $api_name_prefix . '.' . $api_name . ' ' . $item->comparator . $value;
-        }
-        return $output;
-    }
+//    public function formatArrayValue($arrayItems, &$output): string
+//    {
+//        $output .= '(';
+//        foreach ($arrayItems as $arrayIndex => $item) {
+//            $output = $this->getString($item, $output);
+//            if (is_array($item)) {
+//                $this->formatArrayValue($item, $output);
+//            }
+//
+//        }
+//        $output .= ')';
+//
+//        return $output;
+//    }
+//
+//
+//    public function getString($item, string $output): string
+//    {
+//        if (is_string($item)) {
+//            $output .= ' ' . $item . ' ';
+//        }
+//        if (is_object($item)) {
+//            $api_name_prefix = self::API_NAME_PREFIX[$item->api_name] ?? $item->api_name;
+//            $api_name = self::API_NAME[$item->api_name] ?? $item->api_name;
+//            $value = $item->comparator == 'contains' ? ' ' . '\'%' . $item->value . '%\'' : '';
+//
+//            $output .= $api_name_prefix . '.' . $api_name . ' ' . $item->comparator . $value;
+//        }
+//        return $output;
+//    }
 
 
 }
